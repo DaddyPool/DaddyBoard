2fecd9cb1ce8243974ebc1df4efb133879e4f9e6
MijinKo1127 <kasoutuukamoon55@gmail.com>
2019-09-11 18:39:14 +0900

    ボタン押したときに丸を表示＆全ボタンが丸の時に大丸（花丸予定）を表示するように変更

--- app/src/main/java/com/daddypooldev/daddyboard/MainActivity.java
+++ app/src/main/java/com/daddypooldev/daddyboard/MainActivity.java
@@ -7,11 +7,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+
+    //ボタンの状態変数の宣言
+    boolean onbutton1=false;
+    boolean onbutton2=false;
+    boolean onbutton3=false;
+    boolean onbutton4=false;
+    boolean onbutton5=false;
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -33,6 +42,8 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,13 +53,48 @@ public class MainActivity extends AppCompatActivity {
 //        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
 //        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-        Button button = this.findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener(){
-           @Override
-            public void onClick(View v) {
-
-            }
-        });
+//        Button button = this.findViewById(R.id.button);
+//        button.setOnClickListener(new View.OnClickListener(){
+//           @Override
+//            public void onClick(View v) {
+//
+//            }
+//        });
     }
-
+    // クリック時に呼ばれるメソッド
+    public void onButtonClick(View view){
+        switch (view.getId()) {
+            case R.id.button:
+                final ImageView maruImage1 = (ImageView) this.findViewById(R.id.maru1);
+                maruImage1.setVisibility(View.VISIBLE);
+                onbutton1 = true;
+                break;
+            case R.id.button2:
+                final ImageView maruImage2 = (ImageView) this.findViewById(R.id.maru2);
+                maruImage2.setVisibility(View.VISIBLE);
+                onbutton2 = true;
+                break;
+            case R.id.button3:
+                final ImageView maruImage3 = (ImageView) this.findViewById(R.id.maru3);
+                maruImage3.setVisibility(View.VISIBLE);
+                onbutton3 = true;
+                break;
+            case R.id.button4:
+                final ImageView maruImage4 = (ImageView) this.findViewById(R.id.maru4);
+                maruImage4.setVisibility(View.VISIBLE);
+                onbutton4 = true;
+                break;
+            case R.id.button5:
+                final ImageView maruImage5 = (ImageView) this.findViewById(R.id.maru5);
+                maruImage5.setVisibility(View.VISIBLE);
+                onbutton5 = true;
+                break;
+            case R.id.button6:
+                if ((onbutton1==true)&&(onbutton2==true)&&(onbutton3==true)&&(onbutton4==true)&&(onbutton5==true)) {
+                    final ImageView daimaruImage1 = (ImageView) this.findViewById(R.id.daimaru1);
+                    daimaruImage1.setVisibility(View.VISIBLE);
+                    break;
+                }
+        }
+    }
 }
